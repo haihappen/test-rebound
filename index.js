96596cdddb51653e51b8b3430529d95f966908ed
Mario Uher <uher.mario@gmail.com>
2016-03-11 01:52:06 +0100

    Add working example

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import rebound from "rebound";
 
 
-ReactDOM.render(<h1>Make something!</h1>, document.querySelector("main"));
+class Header extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { value: 0 };
+  }
+
+
+  componentDidMount() {
+    const springSystem = new rebound.SpringSystem();
+    const spring = springSystem.createSpring();
+
+    spring.addListener(this);
+    spring.setEndValue(1);
+  }
+
+
+  onSpringUpdate(spring) {
+    this.setState({ value: spring.getCurrentValue() });
+  }
+
+
+  render() {
+    return (
+      <h1>{this.state.value}</h1>
+    );
+  }
+}
+
+
+ReactDOM.render(<Header>Make something!</Header>, document.querySelector("main"));
